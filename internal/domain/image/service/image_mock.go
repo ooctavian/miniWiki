6b31f3356ddfd0db3ab1a11f6f80564a51061e41
Octavian <octaviancir@gmail.com>
2023-03-29 17:31:21 +0300

    fix pipeline

--- internal/domain/image/service/image_mock.go
+++ internal/domain/image/service/image_mock.go
@@ -13,12 +13,12 @@ type ImageMock struct {
 	mock.Mock
 }
 
-func (m ImageMock) Upload(_ context.Context, request model.UploadRequest) error {
+func (m *ImageMock) Upload(_ context.Context, request model.UploadRequest) error {
 	args := m.Called(request)
 	return args.Error(0)
 }
 
-func (m ImageMock) Download(_ context.Context, request model.DownloadRequest) (io.Reader, error) {
+func (m *ImageMock) Download(_ context.Context, request model.DownloadRequest) (io.Reader, error) {
 	args := m.Called(request)
 	return args.Get(0).(io.Reader), args.Error(1)
 }
--- pkg/security/hash_mock.go
+++ pkg/security/hash_mock.go
@@ -6,17 +6,17 @@ type HashMock struct {
 	mock.Mock
 }
 
-func (m HashMock) GenerateFormatted(password string) (string, error) {
+func (m *HashMock) GenerateFormatted(password string) (string, error) {
 	args := m.Called(password)
 	return args.String(0), args.Error(1)
 }
 
-func (m HashMock) GenerateRaw(password string) ([]byte, []byte, error) {
+func (m *HashMock) GenerateRaw(password string) ([]byte, []byte, error) {
 	args := m.Called(password)
 	return args.Get(0).([]byte), args.Get(1).([]byte), args.Error(2)
 }
 
-func (m HashMock) Equal(password string, hash string) (bool, error) {
+func (m *HashMock) Equal(password string, hash string) (bool, error) {
 	args := m.Called(password, hash)
 	return args.Bool(0), args.Error(1)
 }
