4c64e7d8858dd57c90545c6b73d9f1cf1d21d915
Octavian <octaviancir@gmail.com>
2023-04-14 22:33:21 +0300

    fix linting

--- internal/domain/resource/service/mock_test.go
+++ internal/domain/resource/service/mock_test.go
@@ -14,32 +14,32 @@ type resourceRepositoryMock struct {
 	mock.Mock
 }
 
-func (m resourceRepositoryMock) GetResourceById(_ context.Context, id int) (*model.Resource, error) {
+func (m *resourceRepositoryMock) GetResourceById(_ context.Context, id int) (*model.Resource, error) {
 	args := m.Called(id)
 	return args.Get(0).(*model.Resource), args.Error(1)
 }
 
-func (m resourceRepositoryMock) GetResources(_ context.Context, accountId int, pagination utils.Pagination, filters model.GetResourcesFilters) (utils.Pagination, error) {
+func (m *resourceRepositoryMock) GetResources(_ context.Context, accountId int, pagination utils.Pagination, filters model.GetResourcesFilters) (utils.Pagination, error) {
 	args := m.Called(accountId, pagination, filters)
 	return args.Get(0).(utils.Pagination), args.Error(1)
 }
 
-func (m resourceRepositoryMock) DeleteResourceById(_ context.Context, resourceId uint, accountId uint) error {
+func (m *resourceRepositoryMock) DeleteResourceById(_ context.Context, resourceId uint, accountId uint) error {
 	args := m.Called(resourceId, accountId)
 	return args.Error(0)
 }
 
-func (m resourceRepositoryMock) InsertResource(_ context.Context, resource model.CreateResource) (uint, error) {
+func (m *resourceRepositoryMock) InsertResource(_ context.Context, resource model.CreateResource) (uint, error) {
 	args := m.Called(resource)
 	return args.Get(0).(uint), args.Error(1)
 }
 
-func (m resourceRepositoryMock) UpdateResource(_ context.Context, request model.UpdateResourceRequest) error {
+func (m *resourceRepositoryMock) UpdateResource(_ context.Context, request model.UpdateResourceRequest) error {
 	args := m.Called(request)
 	return args.Error(0)
 }
 
-func (m resourceRepositoryMock) UpdateResourcePicture(_ context.Context, resourceId int, accountId int, path string) error {
+func (m *resourceRepositoryMock) UpdateResourcePicture(_ context.Context, resourceId int, accountId int, path string) error {
 	args := m.Called(resourceId, accountId, path)
 	return args.Error(0)
 }
@@ -48,7 +48,7 @@ type categoryServiceMock struct {
 	mock.Mock
 }
 
-func (m categoryServiceMock) CreateCategory(_ context.Context, request model2.CreateCategoryRequest) (*int, error) {
+func (m *categoryServiceMock) CreateCategory(_ context.Context, request model2.CreateCategoryRequest) (*int, error) {
 	args := m.Called(request)
 	return args.Get(0).(*int), args.Error(1)
 }
